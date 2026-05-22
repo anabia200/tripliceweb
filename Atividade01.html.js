0570cb03cef2fa87ded34c25e1b18550e1106747
Bia <wranabia@gmail.com>
2026-05-21 21:13:21 -0300

    definiçao da cor do botao

--- Atividade01.html.js
+++ Atividade01.html.js
@@ -3,19 +3,30 @@ const elementoForm = document.getElementById('formulario');
 elementoForm.addEventListener('submit', (e) => e.preventDefault());
 
 const elementCor = document.getElementById('cor');
-
 elementCor.addEventListener('input', (e ) => {
     let corNova = e.target.value;
+
+    localStorage.setItem('primaryColor', corNova);
 
     document.documentElement.style.setProperty('--primary-color', corNova);
 });
 
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    const rootStyles = window.getComputedStyle(document.documentElement);
+    if (localStorage.getItem('primaryColor')){
 
-    const primaryColor = rootStyles.getPropertyValue('--primary-color').trim();
+        elementCor.value = localStorage.getItem
+        ('primaryColor');
+        document.documentElement.style.setProperty('--primary-color', elementCor.value);
+    }else {
+        const rootStyles = window.getComputedStyle(document.documentElement);
 
-    elementoCor.valeu = primaryColor;
+        const primaryColor = rootStyles.getPropertyValue
+        ('--primary-color').trim();
 
+        elementCor.value = primaryColor;
 
+    }
 })
